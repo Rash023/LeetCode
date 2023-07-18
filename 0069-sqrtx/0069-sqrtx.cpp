216d9b7a6ed4632e6f1816b129becd11c1759a9e
Rashid Mazhar <91797681+Rash023@users.noreply.github.com>
2023-07-18 15:49:47 +0530

    Time: 3 ms (61.49%), Space: 5.9 MB (90.04%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    long long int bsearch(int x) {
+        int s=0;
+        int e=x;
+        long long int mid= s + (e-s)/2;
+        int ans=0;
+        while(s<=e){
+            long long int square=mid*mid;
+            if(square==x){
+                return mid;
+
+            }
+            else if(square>x){
+                e=mid-1;
+            }
+
+            else if(square<x){
+                ans=mid;
+                s=mid+1;
+            }
+            mid= s + (e-s)/2;
+
+        }
+
+        return ans;
+
+        
+    }
+
+    int mySqrt(int x){
+
+        return bsearch(x);
+
+    }
+
+
+
+    
+    
+};
