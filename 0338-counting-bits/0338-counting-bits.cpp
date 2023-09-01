5612b99cb17ce38e9e84c9793399fe55471a49d4
Rashid Mazhar <91797681+Rash023@users.noreply.github.com>
2023-09-01 08:34:34 +0530

    Time: 6 ms (56.68%), Space: 8.7 MB (5.98%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,25 +1,20 @@
 class Solution {
-private:
-    int solve(int n){
-        if(n==1){
-            return 1;
-        }
-        if(n==0){
-            return 0;
-        }
-        
-        if(n%2==0)
-            return solve(n/2);
-        else
-            return 1+solve(n/2);
-    }
     
 public:
     vector<int> countBits(int n) {
         vector<int> ans;
-        int bits=0;
-      for(int i=0;i<=n;i++){
-          ans.push_back(solve(i));
+        ans.push_back(0);
+        if(n==0)
+            return ans;
+    
+      for(int i=1;i<=n;i++){
+          if(i%2==0){
+              ans.push_back(ans[i/2]);
+              
+          }
+          else{
+              ans.push_back(ans[i-1]+1);
+          }
           
       }  
         
