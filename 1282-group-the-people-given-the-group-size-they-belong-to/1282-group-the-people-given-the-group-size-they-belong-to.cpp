42ac6d769a925e3c008ae995d619c44a389b564b
Rashid Mazhar <91797681+Rash023@users.noreply.github.com>
2023-09-11 09:32:18 +0530

    Time: 55 ms (5.22%), Space: 19.6 MB (5.29%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -1,23 +1,25 @@
 class Solution {
- public:
-  vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
-    vector<vector<int>> ans;
-    unordered_map<int, vector<int>> groupSizeToIndices;
-
-    for (int i = 0; i < groupSizes.size(); ++i)
-      groupSizeToIndices[groupSizes[i]].push_back(i);
-
-    for (const auto& [groupSize, indices] : groupSizeToIndices) {
-      vector<int> groupIndices;
-      for (const int index : indices) {
-        groupIndices.push_back(index);
-        if (groupIndices.size() == groupSize) {
-          ans.push_back(groupIndices);
-          groupIndices.clear();
+public:
+    vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
+        vector<vector<int>> ans;
+        if(groupSizes.size()==1){
+            ans.push_back({0});
+            return ans;
         }
-      }
+        for(int i=0;i<groupSizes.size();i++){
+            vector<int> temp;
+            temp.push_back(i);
+            for(int j=i+1;j<groupSizes.size();j++){
+                if(groupSizes[j]==groupSizes[i] && temp.size()<groupSizes[i]){
+                    temp.push_back(j);
+                    groupSizes[j]=-1;
+                }
+                
+            }
+            if(temp.size()==groupSizes[i]){
+                ans.push_back(temp);
+            }
+        }
+        return ans;
     }
-
-    return ans;
-  }
 };
