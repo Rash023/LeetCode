cc4402b83ddf4dff87ff2a780df4ec7a2ddbae34
Rashid Mazhar <91797681+Rash023@users.noreply.github.com>
2023-09-25 07:44:08 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (8.68%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,20 +1,10 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        if(s.size()==0){
-            return t[0];
+        for(int i=0;i<s.size();i++){
+            t[i+1]+=t[i]-s[i]; //passing the diff to the next ele
         }
-        string ans;
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        for(int i=0;i<t.size();i++){
-            ans+=t[i];
-            for(int j=0;j<ans.size();j++){
-                if(ans[j]!=s[j] || j>s.size()){
-                    return ans[j];
-                }
-            }
-        }
-        return t[t.size()-1];
+        
+        return t[t.size()-1]; //eventually the diff will be stored in the last ele
     }
 };
