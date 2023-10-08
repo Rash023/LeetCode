5953d79effc766dacd3a0ee1a755bfbddcc864bb
Rashid Mazhar <91797681+Rash023@users.noreply.github.com>
2023-10-08 12:19:29 +0530

    Time: 2 ms (93.78%), Space: 7.2 MB (52.55%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        int hash[256]={0};
-        bool isMapped[256]={0};
-        for(int i=0;i<s.size();i++){
-            if(hash[s[i]]==0 && isMapped[t[i]]==0){
-                hash[s[i]]=t[i];
-                isMapped[t[i]]=true;
+        int hash[256] = {0};
+        bool isTmapped[256] = {0};
+
+        for(int i = 0; i < s.size(); i++) {
+            if(hash[s[i]] == 0 && isTmapped[t[i]]==0) {
+                hash[s[i]] = t[i];
+                isTmapped[t[i]] = true;
             }
-            
         }
-        
-        for(int i=0;i<t.size();i++){
-            if(char(hash[s[i]])!=t[i]){
+        for(int i = 0; i < t.size(); i++) {
+            if(char(hash[s[i]]) != t[i]) {
                 return false;
-                
-            }
+            } 
         }
         return true;
     }
