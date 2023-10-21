1d3cb2576e8875e6e8f7dc66bb6709560e74338b
Rashid Mazhar <91797681+Rash023@users.noreply.github.com>
2023-10-21 16:42:37 +0530

    Time: 0 ms (100.00%), Space: 7 MB (50.68%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
-    string addRE(string num1,int p1,string num2,int p2,int carry=0){
+    void addRE(string &num1,int p1,string &num2,int p2,int carry,string &ans){
         //base case
         if(p1<0 && p2<0){
             if(carry!=0){
-                return string(1,carry+'0');
+                ans.push_back(carry+'0');
+                
             }
-            return "";
+            return ;
         }
         
         //solving 1 case
@@ -15,15 +16,16 @@ public:
         int csum=n1+n2+carry;
         int digit=csum%10;
         carry=csum/10;
-        string ans="";
+      
         ans.push_back(digit+'0');
-        ans+=addRE(num1,p1-1,num2,p2-1,carry);
+        addRE(num1,p1-1,num2,p2-1,carry,ans);
         
-        return ans;
+       
         
     }
     string addStrings(string num1, string num2) {
-        string ans=addRE(num1,num1.size()-1,num2,num2.size()-1);
+        string ans="";
+        addRE(num1,num1.size()-1,num2,num2.size()-1,0,ans);
         reverse(begin(ans),end(ans));
         return ans;
     }
