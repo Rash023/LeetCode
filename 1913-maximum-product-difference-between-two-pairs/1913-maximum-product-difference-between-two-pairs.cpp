c83c7331af6736821d343d5e93913061317b9c02
Rashid Mazhar <91797681+Rash023@users.noreply.github.com>
2023-12-18 13:47:25 +0530

    Time: 24 ms (47.05%), Space: 20.6 MB (64.73%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cpp
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cpp
@@ -4,9 +4,7 @@ public:
         
         sort(begin(nums),end(nums));
         
-        int maxProd=nums[nums.size()-1]*nums[nums.size()-2];
-        int minProd=nums[0]*nums[1];
-        
-        return maxProd-minProd;
+
+        return (nums[nums.size()-1]*nums[nums.size()-2])-(nums[0]*nums[1]);
     }
 };
