54892e327ad98fa61cf72c3f5bc770c86e3cde49
Rashid Mazhar <91797681+Rash023@users.noreply.github.com>
2024-01-02 12:33:10 +0530

    Time: 4 ms (94.78%), Space: 30.1 MB (62.82%) - LeetHub

---
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> findMatrix(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        
+        vector<vector<int>> result;
+        
+        
+        for(int i=0;i<nums.size();i++){
+            int freq=mp[nums[i]];
+            
+            if(freq==result.size()){
+                result.push_back({}); //creating new row;
+                
+            }
+            
+            result[freq].push_back(nums[i]);
+            
+            mp[nums[i]]++;
+        }
+        
+        return result;
+    }
+};
