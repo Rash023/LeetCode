169c793e8506a3dd58ec4b0cfc8b39a3e19c75b0
Rashid Mazhar <91797681+Rash023@users.noreply.github.com>
2024-01-14 20:15:16 +0530

    Time: 45 ms (86.32%), Space: 19 MB (38.98%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
@@ -3,43 +3,41 @@ class Info{
     int data;
     int rowIndex;
     int colIndex;
+    
     Info(int data,int rowIndex,int colIndex){
         this->data=data;
         this->rowIndex=rowIndex;
         this->colIndex=colIndex;
+        
     }
 };
 
 
 class compare{
     public:
-    bool operator()(Info  *a,Info * b){
+    bool operator()(Info *a,Info *b){
         return a->data>b->data;
+        
     }
 };
+
 
 
 class Solution {
 public:
     vector<int> smallestRange(vector<vector<int>>& nums) {
-        priority_queue<Info *,vector<Info*>,compare> pq;
-        
+        priority_queue<Info*,vector<Info *>,compare> pq;
         int maxi=INT_MIN;
         int mini=INT_MAX;
-        
-        //proces first k elements 
         
         for(int i=0;i<nums.size();i++){
             int element=nums[i][0];
             int rowIndex=i;
             int colIndex=0;
-            
             Info *info=new Info(element,rowIndex,colIndex);
-            
             pq.push(info);
             maxi=max(maxi,element);
             mini=min(mini,element);
-            
             
         }
         
@@ -47,41 +45,33 @@ public:
         int ansEnd=maxi;
         
         while(!pq.empty()){
-            Info *newInfo=pq.top();
+            Info *topNode=pq.top();
             pq.pop();
-            int element=newInfo->data;
-            int rowIndex=newInfo->rowIndex;
-            int colIndex=newInfo->colIndex;
+            int topElement=topNode->data;
+            int topRow=topNode->rowIndex;
+            int topCol=topNode->colIndex;
             
-            int mini=newInfo->data;
+            int mini=topNode->data;
+            
             
             if((maxi-mini)<(ansEnd-ansStart)){
-                ansStart=mini;
                 ansEnd=maxi;
-                
-                
+                ansStart=mini;
             }
             
-            if(colIndex+1<nums[rowIndex].size()){
-                int newElement=nums[rowIndex][colIndex+1];
-                
-                maxi=max(maxi,newElement);
-                
-                Info *topNode=new Info(newElement,rowIndex,colIndex+1);
-                pq.push(topNode);
-                
+            if(topCol+1<nums[topRow].size()){
+                int element=nums[topRow][topCol+1];
+                maxi=max(maxi,element);
+                Info *newInfo=new Info(element,topRow,topCol+1);
+                pq.push(newInfo);
             }
-            
             else{
                 break;
-                
+
             }
-            
-            
         }
         
-        return {ansStart,ansEnd};
-        
+        return{ansStart,ansEnd};
         
     }
 };
