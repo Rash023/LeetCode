d6aa08bb5f0bae298587249842c650863f8ac5d3
Rashid Mazhar <91797681+Rash023@users.noreply.github.com>
2024-02-10 20:26:23 +0530

    Time: 36 ms (21.57%), Space: 11.8 MB (53.19%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,36 +1,28 @@
 class Solution {
 public:
     int t[1001][1001];
-    
+     
     bool check(int i,int j,string &s){
         if(i>j) return true;
         
-        if(t[i][j]!=-1){
-            return t[i][j];
-        }
-        
-        
+        if(t[i][j]!=-1) return t[i][j];
         if(s[i]==s[j]){
-            return t[i][j]= check(i+1,j-1,s);
+            return t[i][j]=check(i+1,j-1,s);
+            
         }
-        
         
         return t[i][j]=false;
+        
     }
     
     int countSubstrings(string s) {
         int ans=0;
         memset(t,-1,sizeof(t));
-        
         for(int i=0;i<s.size();i++){
             for(int j=i;j<s.size();j++){
                 if(check(i,j,s)){
                     ans++;
                 }
-                
-                
-                
-                
             }
         }
         
