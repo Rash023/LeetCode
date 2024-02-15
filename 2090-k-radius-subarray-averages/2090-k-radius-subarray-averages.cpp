302eb87b94639e473aa4092e927c407c8dd34da4
Rashid Mazhar <91797681+Rash023@users.noreply.github.com>
2024-02-15 22:39:37 +0530

    Time: 133 ms (94.03%), Space: 141.4 MB (20.21%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
@@ -3,29 +3,27 @@ public:
     vector<int> getAverages(vector<int>& nums, int k) {
         int n=nums.size();
         int len=2*k+1;
+        
         vector<int> res(n,-1);
         
         vector<long> preSum(n+1);
         
         if(n<len){
             return res;
+            
         }
-        
-        
         
         for(int i=0;i<n;i++){
             preSum[i+1]=preSum[i]+nums[i];
             
         }
         
-        for(int i=k;i+k<n;++i){
+        for(int i=k;i+k<n;i++){
             res[i]=(preSum[i+k+1]-preSum[i-k])/len;
             
         }
         
         return res;
-        
-        
         
     }
 };
