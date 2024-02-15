eb67bf471b17892a299cbe45c23a7a2a4d1ece96
Rashid Mazhar <91797681+Rash023@users.noreply.github.com>
2024-02-15 20:35:57 +0530

    Time: 107 ms (47.55%), Space: 59.5 MB (15.66%) - LeetHub

--- 3031-minimum-time-to-revert-word-to-initial-state-ii/3031-minimum-time-to-revert-word-to-initial-state-ii.cpp
+++ 3031-minimum-time-to-revert-word-to-initial-state-ii/3031-minimum-time-to-revert-word-to-initial-state-ii.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-    void computeLPS(string &word,vector<int> &lps){
-        int m=word.size();
-        
-        int len=0;
+    void LPS(string &word,vector<int> &lps,int n){
         lps[0]=0;
-        int i=1;
         
-        while(i<m){
+        int i=1;
+        int len=0;
+        
+        while(i<n){
             if(word[i]==word[len]){
                 len++;
                 lps[i]=len;
                 i++;
+                
             }
             else{
                 if(len!=0){
                     len=lps[len-1];
-               
+                    
                 }
                 else{
                     lps[i]=0;
                     i++;
-                    
                 }
             }
         }
@@ -31,25 +30,24 @@ public:
         
         vector<int> lps(n);
         
-        computeLPS(word,lps);
+        LPS(word,lps,n);
         
         int suffix_length=lps[n-1];
         
-        while( suffix_length>0 && (n-suffix_length)%k!=0){
+        while(suffix_length>0 && ((n-suffix_length)%k)!=0){
             suffix_length=lps[suffix_length-1];
             
         }
         
-        
         if((n-suffix_length)%k==0){
             return (n-suffix_length)/k;
+            
+            
             
         }
         else{
             return ceil(n/(double)k);
             
         }
-        
-        
     }
 };
