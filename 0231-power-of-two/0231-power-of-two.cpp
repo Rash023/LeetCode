4bc10d744730492451da85916da3ff0669b51b4d
Rashid Mazhar <91797681+Rash023@users.noreply.github.com>
2024-02-20 22:14:53 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (19.56%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,12 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n==0){
-            return false;
-        }
+        return n>0? !(n&(n-1)):0;
         
-        while(n%2==0) n/=2;
-        
-        return n==1; 
     }
 };
