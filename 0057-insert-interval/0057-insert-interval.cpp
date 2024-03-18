8d2e8492cb5d747c2b92cc64b722749229367ef0
Rashid Mazhar <91797681+Rash023@users.noreply.github.com>
2024-03-18 15:02:03 +0530

    Time: 17 ms (21.46%), Space: 20.6 MB (30.14%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,41 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        if(intervals.size()==0){
-            return {newInterval};
-            
-        }
-        int i=0;
         vector<vector<int>> ans;
+        if(intervals.size()==0) return {newInterval};
+        
+        
+        
+        int i=0;
+        
         while(i<intervals.size()){
             int start=intervals[i][0];
             int end=intervals[i][1];
             
             if(end<newInterval[0]){
-                ans.push_back({intervals[i][0],intervals[i][1]});
+                ans.push_back({start,end});
                 
-        
             }
             else if(start>newInterval[1]){
                 break;
                 
             }
             else{
-                //merging the intervals
-                newInterval[0]=min(newInterval[0],intervals[i][0]);
-                newInterval[1]=max(newInterval[1],intervals[i][1]);
-            
-                
-                
-                
+                newInterval[0]=min(newInterval[0],start);
+                newInterval[1]=max(newInterval[1],end);
                 
             }
             i++;
             
         }
         
-        
         ans.push_back(newInterval);
+        
+        //pushing the rest of the elements back into the arrya
         
         while(i<intervals.size()){
             ans.push_back({intervals[i][0],intervals[i][1]});
@@ -44,7 +40,5 @@ public:
         }
         
         return ans;
-        
-        
     }
 };
